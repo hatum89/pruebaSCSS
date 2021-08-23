e71d0781a0d198e107c868cfa7a088ebc5dd4c14
juan <juanhatumvarela@gmail.com>
2021-08-23 00:31:17 -0500

    primer intento

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./body.component.scss']
 })
 export class BodyComponent implements OnInit {
-
+  change: boolean;
+  activeIconData: number;
   constructor() { }
 
   ngOnInit(): void {
+    this.activeIconData = 1;
   }
 
+  // tslint:disable-next-line:typedef
+  activeIcon() {
+    this.activeIconData = 0;
+    setTimeout(() => {
+      this.activeIconData = 1;
+    }, 200);
+  }
 }
